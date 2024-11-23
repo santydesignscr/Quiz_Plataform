8253259b9e96d58b3e00472c35b482b6afdd71f0
Santiago Rodríguez <88201812+santydesignscr@users.noreply.github.com>
2024-11-23 01:19:28 -0600

    Actualizar App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Toaster } from "@/components/ui/toaster";
 import Quiz from './components/Quiz';  // Componente de Quiz
 import UploadQuizForm from './components/UploadQuizForm';  // Componente de subir quiz
 import SearchQuizzes from './components/SearchQuizzes';  // Componente para buscar quizzes
